967f89797c8705688c9cc231fbbbe7501582e628
James Bacon <james@baconi.co.uk>
2020-10-10 20:33:10 +0100

    Build and deploy with error catching.

--- CI/build.ps1
+++ CI/build.ps1
@@ -3,16 +3,44 @@ $UNITY_VERSION = "2019.4.12f1"
 
 function UnityBuild($BuildTarget, $BuildName) {
 
-  Write-Output "Starting build for: $BuildTarget"
+  Write-Host "Starting build for: $BuildTarget" -ForegroundColor Green
 
   & "C:\Program Files\Unity\Hub\Editor\$UNITY_VERSION\Editor\Unity.exe" -batchmode -quit -nographics -buildName $BuildName -buildTarget $BuildTarget -executeMethod Editor.CI.Builder.Build | Write-Output
 
-  # TODO - Check for failures
+  if ($? -eq $false) {
+    Write-Host "Failed $BuildName - $BuildTarget " -ForegroundColor Red 
+    exit 1
+  }
 
-  Write-Output "Finished build for: $BuildTarget"
+  Write-Host "Finished build for: $BuildTarget" -ForegroundColor Green
 }
 
+function ItchIoPublish($BuildTarget, $Project, $Channel) {
+
+  Write-Host "Publishing ${BuildTarget} to ${Project}:${Channel}" -ForegroundColor Green
+  
+  butler push ./Build/${BuildTarget} ${Project}:${Channel}
+
+  if ($? -eq $false) {
+    Write-Host "Failed to publish ${BuildTarget} to ${Project}:${Channel} " -ForegroundColor Red
+    exit 1
+  }
+
+  Write-Host "Published ${BuildTarget} to ${Project}:${Channel}" -ForegroundColor Green
+}
+
+##
+# Build
+##
 UnityBuild -BuildTarget StandaloneWindows64 -BuildName retro-brick-game.exe
 UnityBuild -BuildTarget StandaloneLinux64 -BuildName retro-brick-game
 UnityBuild -BuildTarget StandaloneOSX -BuildName retro-brick-game
 UnityBuild -BuildTarget WebGL -BuildName retro-brick-game
+
+##
+# Publish
+##
+ItchIoPublish -BuildTarget StandaloneWindows64 -Project beercan/retro-brick-game -Channel windows
+ItchIoPublish -BuildTarget StandaloneLinux64 -Project beercan/retro-brick-game -Channel linux
+ItchIoPublish -BuildTarget StandaloneOSX -Project beercan/retro-brick-game -Channel osx
+ItchIoPublish -BuildTarget WebGL -Project beercan/retro-brick-game -Channel webgl
